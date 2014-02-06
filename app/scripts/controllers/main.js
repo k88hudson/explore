ee9136e9a27fe4e45adeefd1861f086761ec5ea3
Gvn Lazar Suntop <gavin@gsuntop.com>
2014-02-06 10:27:32 -0500

    show sidebar links at full opacity on index

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,21 +1,26 @@
 'use strict';
 
 angular.module('exploreApp')
-  .controller('navigationController', function($scope, $location, $routeParams, personaService, Slug, SITE) {
+  .controller('navigationController', function ($scope, $location, $routeParams, personaService, Slug, SITE) {
     $scope.isCollapsed = true;
     $scope.skills = SITE.skills;
-    $scope.isActive = function(skill) {
-      return Slug.slugify(skill) == $routeParams.id;
+
+    $scope.isActive = function (skill) {
+      return Slug.slugify(skill) === $routeParams.id;
     };
+
+    $scope.isUnselected = function () {
+      return window.location.hash === '#/';
+    }
   })
-  .controller('mainController', function($scope) {
+  .controller('mainController', function ($scope) {
 
   })
-  .controller('competencyController', function($scope, $location, $routeParams, Slug, SITE) {
-    $scope.skill = SITE.skills.filter(function(item) {
-      return Slug.slugify(item.name) == $routeParams.id;
+  .controller('competencyController', function ($scope, $location, $routeParams, Slug, SITE) {
+    $scope.skill = SITE.skills.filter(function (item) {
+      return Slug.slugify(item.name) === $routeParams.id;
     })[0];
   })
-  .controller('eventsController', function($scope) {
+  .controller('eventsController', function ($scope) {
     //blah
   });
